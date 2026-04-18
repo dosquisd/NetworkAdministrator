fe49e1e0ad5e9b6c5417ae1ae60d361a8dbe3b98
JuanD <dosquisd123@gmail.com>
2026-04-17 20:58:12 -0500

    feat: configure dhcp server to the homebrew router

--- scripts/create_inventory.py
+++ scripts/create_inventory.py
@@ -2,7 +2,7 @@
 import argparse
 import socket
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -16,6 +16,23 @@ DEFAULT_LAN_INTERFACE = "wlan0"
 DEFAULT_LAN_CIDR = "192.168.1.0/24"
 
 
+def concatenate_binaries(array: List[int], fixed_length: int = 0) -> int:
+    result = 0
+    for byte in array:
+        fixed_length = max(fixed_length, byte.bit_length())
+        result = (result << fixed_length) | byte
+    return result
+
+
+def split_into_octets(value: int) -> List[int]:
+    return list(map(lambda i: (value >> (24 - i * 8)) & 0xFF, range(4)))
+
+
+def binary_to_ip(value: int) -> str:
+    octets = split_into_octets(value)
+    return ".".join(str(octet) for octet in octets)
+
+
 def main(
     *,
     username: str,
@@ -24,6 +41,10 @@ def main(
     wan_interface: str,
     lan_interface: str,
     lan_cidr: str,
+    lan_cidr_start: Optional[str] = None,
+    lan_cidr_end: Optional[str] = None,
+    lan_cidr_gateway: Optional[str] = None,
+    lan_cidr_broadcast: Optional[str] = None,
     template_dir: Optional[str] = None,
     verbose: bool = False,
 ) -> None:
@@ -39,21 +60,66 @@ def main(
     if verbose:
         print(f"[+] Using template directory: {template_dir}")
 
-    output_file = (Path(out_dir) / "inventory.ini").resolve()
-    environment = Environment(loader=FileSystemLoader(template_dir))  # type: ignore
-    template = environment.get_template("inventory.ini.j2")
+    # Get parts of the LAN CIDR for later use in the DHCP configuration
+    assert "/" in lan_cidr, (
+        f"Invalid LAN CIDR format: '{lan_cidr}'. Expected format: 'IP_ADDRESS/SUBNET_MASK_BITS'"
+    )
+
+    ip, subnet_bits = lan_cidr.split("/")
+    subnet_bits = int(subnet_bits)
+    assert 0 < subnet_bits <= 32, (
+        f"Invalid subnet mask bits: {subnet_bits}. Must be between 0 and 32."
+    )
+    assert ip.count(".") == 3, f"Invalid IP address format in LAN CIDR: '{ip}'"
+
+    network_address = concatenate_binaries([int(octet) for octet in ip.split(".")], 8)
+
+    # basically: /24 -> 255.255.255.0
+    mask = 0xFFFFFFFF << (32 - subnet_bits)
+    lan = binary_to_ip(network_address & mask)
+    lan_cidr_netmask = binary_to_ip(mask)
+
+    # When the gateway is not provided, then the first host in the LAN CIDR will be used as the default gateway
+    if lan_cidr_gateway is None:
+        first_host = network_address + 1
+        lan_cidr_gateway = binary_to_ip(first_host)
+
+    # When the gateway is not provided, then the last host in the LAN CIDR will be used as the default gateway
+    if lan_cidr_broadcast is None:
+        last_host = network_address | (0xFFFFFFFF >> subnet_bits)
+        lan_cidr_broadcast = binary_to_ip(last_host)
+
+    # If the start and end of the DHCP range are not provided, then the entire range of hosts in the LAN CIDR will be used,
+    # excluding the network address, gateway, and broadcast address
+    if lan_cidr_start is None:
+        # +2 to skip the network address and the gateway
+        lan_cidr_start = binary_to_ip(network_address + 2)
+
+    if lan_cidr_end is None:
+        lan_cidr_end = binary_to_ip(network_address | (0xFFFFFFFF >> subnet_bits) - 1)
 
     server_info = {
         "ip": ip_address,
         "user": username,
         "wan_interface": wan_interface,
         "lan_interface": lan_interface,
-        "lan_cidr": lan_cidr,
+        "lan_cidr": f"{lan}/{subnet_bits}",
+        "lan": lan,
+        "lan_cidr_netmask": lan_cidr_netmask,
+        "lan_cidr_gateway": lan_cidr_gateway,
+        "lan_cidr_broadcast": lan_cidr_broadcast,
+        "lan_cidr_start": lan_cidr_start,
+        "lan_cidr_end": lan_cidr_end,
     }
 
     if verbose:
-        print(f"[+] Generating inventory content with the following server information: {server_info}")
+        print(
+            f"[+] Generating inventory content with the following server information: {server_info}"
+        )
 
+    output_file = (Path(out_dir) / "inventory.ini").resolve()
+    environment = Environment(loader=FileSystemLoader(template_dir))  # type: ignore
+    template = environment.get_template("inventory.ini.j2")
     final_inventory = template.render(server=server_info)
     if verbose:
         print(f"[+] Generated inventory content. Now writing to file {output_file}...")
@@ -84,6 +150,42 @@ if __name__ == "__main__":
         type=str,
         default=DEFAULT_LAN_CIDR,
         help=f"The CIDR notation for the LAN network (default: {DEFAULT_LAN_CIDR}).",
+    )
+    parser.add_argument(
+        "--lan-cidr-gateway",
+        type=str,
+        default=None,
+        help=(
+            "The IP address of the gateway for the LAN. "
+            "If not provided, it will default to the first host in the LAN CIDR."
+        ),
+    )
+    parser.add_argument(
+        "--lan-cidr-broadcast",
+        type=str,
+        default=None,
+        help=(
+            "The IP address of the broadcast address for the LAN. "
+            "If not provided, it will default to the last host in the LAN CIDR."
+        ),
+    )
+    parser.add_argument(
+        "--lan-cidr-start",
+        type=str,
+        default=None,
+        help=(
+            "The starting IP address of the DHCP range for the LAN. "
+            "If not provided, it will default to the first host in the LAN CIDR after the gateway."
+        ),
+    )
+    parser.add_argument(
+        "--lan-cidr-end",
+        type=str,
+        default=None,
+        help=(
+            "The ending IP address of the DHCP range for the LAN. "
+            "If not provided, it will default to the last host in the LAN CIDR before the broadcast address."
+        ),
     )
     parser.add_argument(
         "--lan-interface",
@@ -123,6 +225,10 @@ if __name__ == "__main__":
         wan_interface=args.wan_interface,
         lan_interface=args.lan_interface,
         lan_cidr=args.lan_cidr,
+        lan_cidr_gateway=args.lan_cidr_gateway,
+        lan_cidr_broadcast=args.lan_cidr_broadcast,
+        lan_cidr_start=args.lan_cidr_start,
+        lan_cidr_end=args.lan_cidr_end,
         template_dir=args.template_dir,
         verbose=args.verbose,
     )
